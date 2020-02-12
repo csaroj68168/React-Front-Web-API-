d1035ea165b0141a4d92c7769e69fa8d4f780611
RaZz Chaudhary <55077982+craj68168@users.noreply.github.com>
2020-02-12 14:43:24 +0545

    routes updated

--- src/Routes.js
+++ src/Routes.js
@@ -27,7 +27,12 @@ const Routes = () => {
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
-             
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+                <AdminRoute path="/create/category" exact component={AddCategory} />
+                <AdminRoute path="/create/product" exact component={AddProduct} />
+                <Route path="/product/:productId" exact component={Product} />
+                <Route path="/cart" exact component={Cart} />
+                <AdminRoute path="/admin/orders" exact component={Orders} />
                 <PrivateRoute path="/profile/:userId" exact component={Profile} />
                 <PrivateRoute path="/admin/products" exact component={ManageProducts} />
                 <AdminRoute path="/admin/product/update/:productId" exact component={UpdateProduct} />
